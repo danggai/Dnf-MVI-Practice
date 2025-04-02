a8c2f48e4c797e8c3e910a9ad6b7e316474aac99
danggai <donggi9313@naver.com>
2025-04-02 18:42:02 +0900

    - Domain 레이어에 네트워크 통신에 필요한 라이브러리 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,3 +7,10 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.coroutines.core)
+    implementation(libs.java.inject)
+    implementation(libs.gson)
+}
