ed6951fa6a162e5cc86c7218eb4cf53ab908672f
danggai <donggi9313@naver.com>
2025-04-07 22:13:13 +0900

    - ViewModel에서 Resource에 접근 가능한 Provider 구현

--- app/src/main/java/com/example/dnf_mvi_practice/module/AppModule.kt
+++ app/src/main/java/com/example/dnf_mvi_practice/module/AppModule.kt
@@ -1,9 +1,13 @@
 package com.example.dnf_mvi_practice.module
 
+import android.content.Context
+import com.example.data.repository.ResourceProviderRepositoryImpl
 import com.example.dnf_mvi_practice.Application
+import com.example.domain.repository.ResourceProviderRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,4 +19,10 @@ object AppModule {
     fun provideApplication(): Application {
         return Application()
     }
+
+    @Singleton
+    @Provides
+    fun provideResourceProvider(
+        @ApplicationContext context: Context
+    ): ResourceProviderRepository = ResourceProviderRepositoryImpl(context)
 }
